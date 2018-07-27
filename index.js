c3646cbb435602ba2094df795ee2f2a704f52c7d
dyuki <unblivion@gmail.com>
2018-07-27 16:41:53 -0300

    testando e testanto

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send("Bem Vindo!!, para mais informações digite /help lá em cima ^");
+});
+
+app.get("/help", (req, res)=>{
+    res.send("Não tem como te ajudar, me disgurpe, mas vc pode digitar /pneu");
+});
+
+app.get("/pneu", (req, res)=>{
+    res.send("Bem Vinda à casa dos pneus, para ver mais 'pneus' digite /mais ");
+});
+
+app.get("/pneu/mais", (req, res)=>{
+    res.send("pneuspneuspneuspneuspneuspneuspneuspneus\npneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneuspneus");
+});
+
 app.listen(3000, ()=> console.log("Servidor inicializado"));
